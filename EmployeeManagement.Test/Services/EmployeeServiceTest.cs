2f214207f36c52f85aef1a812937270954d3a3a0
sanjay pakale <sanjaypakale@gmail.com>
2019-12-10 10:06:56 +0530

    added InMemory EF context testing.

---
+++ EmployeeManagement.Test/Services/EmployeeServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeManagement.Core.Services;
+using EmployeeManagement.Data;
+using EmployeeManagement.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmployeeManagement.Test.Services
+{
+    public class EmployeeServiceTest : InMemoryEmployeeContext
+    {
+        [Fact]
+        public async Task WhenGetEmployeeReturnEmployee()
+        {
+            //Arrange
+            
+            var employeeService = new EmployeeServices(EmployeeContext);
+
+            //Act
+
+            var firstEmployee = await employeeService.GetEmployeeById(1);
+
+            //Assert
+
+            Assert.Equal("Sanjay", firstEmployee.FirstName);
+
+        }
+    }
+}
---
+++ EmployeeManagement.Test/Services/InMemoryEmployeeContext.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace EmployeeManagement.Test.Services
+{
+    public abstract class InMemoryEmployeeContext : IDisposable
+    {
+        public EmployeeContext EmployeeContext { get; private set; }
+
+        protected InMemoryEmployeeContext()
+        {
+            var options = new DbContextOptionsBuilder<EmployeeContext>()
+                .UseInMemoryDatabase(new Guid().ToString())
+                .Options;
+
+            EmployeeContext = new EmployeeContext(options);
+
+            EmployeeContext.Employees.Add(new Employee { Id = 1, FirstName = "Sanjay", LastName = "Pakale", Address = "Action", EmployeeCode = "i706089" });
+
+            EmployeeContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            EmployeeContext.Dispose();
+        }
+    }
+}
